e9b192292e266d2e83b45cf9ff4503dfe46171ab
Andre Nobre <afelipe1405@gmail.com>
2026-02-15 14:14:58 -0300

    correção de bugs de DTO de valores hora

--- src/main/java/com/albin/parkgest/dto/valorhora/ValorHoraRegisterDTO.java
+++ src/main/java/com/albin/parkgest/dto/valorhora/ValorHoraRegisterDTO.java
@@ -1,6 +1,7 @@
 package com.albin.parkgest.dto.valorhora;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class ValorHoraRegisterDTO {
-    @NotBlank
+    @NotNull
     private BigDecimal valorHora;
 
     @NotBlank
